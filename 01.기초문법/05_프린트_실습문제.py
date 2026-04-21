8e5d6f9f464dba93e4e9c375bbf184443bfd33dd
minyung0723@gmail.com <minyung0723@gmail.com>
2026-04-21 12:39:45 +0900

    Initial commit

---
+++ 01.기초문법/05_프린트_실습문제.py
@@ -0,0 +1,12 @@
+'''
+
+'''
+
+name="조민영"
+age=25
+height=172.2
+is_student=True
+
+print(name, age, height, is_student ,sep="\n")
+
+print(f"안녕하세요! 제 이름은{name}이고 나이는 {age}살입니다." , f"제 키는 {height}cm 입니다", f"저는 학생입니다: {is_student}", sep="\n")
