044b2616e165f44648aa1603a30e0ffdff199850
minyung0723@gmail.com <minyung0723@gmail.com>
2026-04-24 18:06:22 +0900

    웹사이트 실습

--- 04_클래스_상속/11_웹사이트실습_과제/db.py
+++ 04_클래스_상속/11_웹사이트실습_과제/db.py
@@ -6,7 +6,7 @@ def DB연결():
         port="3306",
         user="root",
         password="tjee1234",
-        database="instagram_clone"
+        database="mydb"
     )
     # 나중에 conn 또한 연결 문제가 생기거나 할 때 대응을 하기 위하여 conn 이라는
     # 변수 공간에 db 연결 세팅 담아두는 것
