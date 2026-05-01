75578301caf39e1a2eabd648aea3d35baf16958f
minyung0723@gmail.com <minyung0723@gmail.com>
2026-05-01 13:19:41 +0900

    넘파이

--- 09_데이터분석_시각화/01_넘파이.py
+++ 09_데이터분석_시각화/01_넘파이.py
@@ -1,7 +1,23 @@
 '''
 pip install numpy
 
-import numpy as np # np 는 numpy라는 이름이 너무 길어서 np로 줄여서 작성하겠다.
+import numpy as np # np 는 numpy 이름이 너무 길어서 np로 줄여서 작성하겠다.
+
+리스트 계산 for 문 너무 힘듦
+줄여서 마치 하나의 변수들을 다루는 것처럼 하겠다!!!
+
+표준편차 : 데이터들이 평균에서 얼마나 멀리 퍼져 있는지를 나타내는 숫자
+데이터가 얼마나 들쭉날쭉한가요
+표준보다 편차가 얼마나 나나요??
+std = STanDard deviation
+
+median = 영어로 중앙값 전체 데이터를 크기 순으로 줄 세웠을 때 정중앙에 있는 값
+
+mean vs avg
+모두 평균
+avg = average 일상 영어에서 사용하는 표현
+mean = 통계/수학/프로그래밍에서 사용
+sql에서는 avg() 분야별로 사용하고자하는 명칭을 개발자가 다르게 생각하고 기능을 만듦
 '''
 
 import numpy as np
@@ -9,18 +25,18 @@ import numpy as np
 
 def 기본넘파이기능():
     # 일반 파이썬 숫자 목록
-    기본파이썬 = [1,2,3,4,5]
+    기본파이썬 = [1, 2, 3, 4, 5]
     # np 로 만들어진 array = 배열이다.
-    넘파이 = np.array([1,2,3,4,5])
+    넘파이 = np.array([1, 2, 3, 4, 5])
 
-    print(type(기본파이썬)) # <class 'list'>
-    print(type(넘파이))     # <class 'numpy.ndarray'>
+    print(type(기본파이썬))  # <class 'list'>
+    print(type(넘파이))  # <class 'numpy.ndarray'>
 
-    # Numpy로 자주 사용하는 배열 생성 방법
-    영_5개 = np.zeros(5)
-    일_5개 = np.ones(5)
-    이_5개 = np.arange(0, 10, 2)
-    균등간격_5개 = np.linspace(0, 1, 5)
+    # Numpy 로 자주 사용하는 배열 생성 방법
+    영_5개 = np.zeros(5)  # [0. 0. 0. 0. 0.]          원하는 개수만큼 0만 있는 목록 만들 때 사용
+    일_5개 = np.ones(5)  # [1. 1. 1. 1. 1.]          원하는 개수만큼 1만 있는 목록 만들 때 사용
+    이_5개 = np.arange(0, 10, 2)  # [0 2 4 6 8]                0부터 10-1 까지 2씩 건너뛰어 생성
+    균등간격_5개 = np.linspace(0, 1, 5)  # [0.   0.25 0.5  0.75 1.  ] 실수형태로 0부터 1까지 n만큼 균등하게 생성
 
     print(영_5개)
     print(일_5개)
@@ -29,70 +45,82 @@ def 기본넘파이기능():
 
 
 def 파이썬과넘파이_연산비교():
-    a_python = [1,2,3]
-    b_python = [4,5,6]
+    a_python = [1, 2, 3]
+    b_python = [4, 5, 6]
 
-    #일반파이썬은 더하기가 이어붙이기 이어붙이는 것은 가능하지만 셈 계산 안됨
-    print(a_python + b_python)
+    # 일반파이썬은 더하기가 이어붙이기 이어붙이는 것은 가능하지만 셈 계산 안됨
+    print(a_python + b_python)  # [1, 2, 3, 4, 5, 6]
 
     # NumPy 수학적 연산
-    a_numpy = np.array([1,2,3])
-    b_numpy = np.array([4,5,6])
+    a_numpy = np.array([1, 2, 3])
+    b_numpy = np.array([4, 5, 6])
+
     더하기 = a_numpy + b_numpy
     빼기 = a_numpy - b_numpy
     곱하기 = a_numpy * b_numpy
     나누기 = a_numpy / b_numpy
-    print("넘파이 더하기 : ", 더하기)
-    print("넘파이 빼기 : ", 빼기)
-    print("넘파이 곱하기 : ", 곱하기)
-    print("넘파이 나누기 : ", 나누기)
-# 파이썬과넘파이_연산비교()
+
+    print("넘파이 더하기 : ", 더하기)  # 넘파이 더하기 :  [5 7 9]
+    print("넘파이 빼기 : ", 빼기)  # 넘파이 빼기 :  [-3 -3 -3]
+    print("넘파이 곱하기 : ", 곱하기)  # 넘파이 곱하기 :  [ 4 10 18]
+    print("넘파이 나누기 : ", 나누기)  # 넘파이 나누기 :  [0.25 0.4  0.5 ]
+
 
 def for문연산비교():
-    a_python = [1,2,3]
+    a_python = [1, 2, 3]
     # 일반 for 문
     for i in a_python:
         print(i * 2)
 
-    # 스칼라 연산
+    # 리스트 컴프리핸션
+    #      결과 for 과정
     print([i * 2 for i in a_python])
 
     # 리스트 연산을 마치 기본 연산처럼 다룰 수 있게 기능 특화되어 있는 라이브러리
-    a_numpy = np.array([1,2,3])
+    a_numpy = np.array([1, 2, 3])
     print(a_numpy * 2)
 
+
+# 기본 파이썬과 비교없이 넘파이 기능만 작성
 def 인덱싱_슬라이싱():
-    # 기본 파이썬과 비교없이 넘파이 기능만 작성
-    a = np.array([10,20,30,40,50])
+    a = np.array([10, 20, 30, 40, 50])
 
     # 기본 인덱싱(파이썬 리스트랑 동일)
-    print("a[0] : ", a[0])
-    print("a[-1] : ", a[-1])
-    print("a[1:3] : ", a[1:3])
-    print("a[a > 25] : ", a[a > 25])
-    print("a[a % 20 == 0] : ", a[a % 20 == 0])
-# 인덱싱_슬라이싱()
+    print("a[0] : ", a[0])  # a[0]           :  10
+    print("a[-1] : ", a[-1])  # a[-1]          :  50
+    print("a[1:3] : ", a[1:3])  # a[1:3]         :  [20 30]
+    # 조건 필터링 NumPy 만의 기능
+    # a > 25       [10, 20, 30, 40, 50] 에서 25 보다 큰 숫자만 걸러내어 a 안에 담아두겠다.
+    print("a[a > 25] : ", a[a > 25])  # a[a > 25]      :  [30 40 50]
+    # a % 20 == 0   [10, 20, 30, 40, 50] 에서  20으로 나누어 떨어지는 숫자만 a 안에 담아두겠다.
+    print("a[a % 20 == 0] : ", a[a % 20 == 0])  # a[a % 20 == 0] :  [20 40]
+
 
 def 조건필터비교():
-    a_numpy = np.array([10,20,30,40,50])
-    넘파이_필터처리 = a_numpy[a_numpy > 10] # 위 리스트에서 10보다 큰 수만 남게된다.
+    a_numpy = np.array([10, 20, 30, 40, 50])
+    넘파이_필터처리 = a_numpy[a_numpy > 10]  # 위 리스트에서 10보다 큰 수만 남게된다.
 
     # 일반 파이썬 필터처리
-    a_python = [10,20,30,40,50]
+    a_python = [10, 20, 30, 40, 50]
     for i in a_python:
         if i > 10:
-            print("10보다 큰 숫자들 : ",i)
+            print("10보다 큰 숫자들 : ", i)
 
     # 일반 파이썬 컴프리핸션
-    # 결과
+    #     결과        작업과정       작업과정 중 지켜야할 조건
+    # [    i     for i in a_python          if i > 10          ]
     [i for i in a_python if i > 10]
 
-def 넘파이_유용한_통계함수():
-    a = np.array([1,2,3,4,5])
 
-    np.sum(a)
-    np.max(a)
-    np.min(a)
-    np.mean(a)
-    np.std(a)
-    np.median(a)
+def 넘파이_유용한_통계함수():
+    a = np.array(([1, 2, 3, 4, 5]))
+
+    np.sum(a)      # 리스트 내 총 합
+    np.max(a)      # 리스트에서 제일 큰 값
+    np.min(a)     # 리스트에서 제일 작은 값
+    np.mean(a)    # 평균
+    np.std(a)     # 표준 편차
+    np.median(a)  # 중앙값
+
+
+넘파이_유용한_통계함수()
