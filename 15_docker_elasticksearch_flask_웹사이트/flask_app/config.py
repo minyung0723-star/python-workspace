0331c16c24ef530a4b49b798757d1dbb50d08c9d
minyung0723@gmail.com <minyung0723@gmail.com>
2026-05-14 11:42:41 +0900

    도커 / 엘라스틱 서치

---
+++ 15_docker_elasticksearch_flask_웹사이트/flask_app/config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    MYSQL_USER     = os.getenv("MYSQL_USER",     "appuser")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "apppass")
+    MYSQL_HOST     = os.getenv("MYSQL_HOST",     "localhost")
+    MYSQL_PORT     = os.getenv("MYSQL_PORT",     "3308")
+    MYSQL_DB       = os.getenv("MYSQL_DB",       "userdb")
+
+    SQLALCHEMY_DATABASE_URI = (
+        # f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ES_HOST  = os.getenv("ES_HOST", "localhost")
+    ES_PORT  = os.getenv("ES_PORT", "9200")
+    ES_URL   = f"http://{ES_HOST}:{ES_PORT}"
+    ES_INDEX = "users"
+
+    DEBUG = os.getenv("FLASK_DEBUG", "true").lower() == "true"
+    HOST  = "0.0.0.0"
+    PORT  = int(os.getenv("PORT", 5000))
